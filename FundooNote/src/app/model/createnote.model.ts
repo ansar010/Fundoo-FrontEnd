182cabe391f367ec250e72bb0a233b932f5cd42f
ansar010 <ansaruddeen030@gmail.com>
2019-03-15 19:24:53 +0530

    15march

---
+++ FundooNote/src/app/model/createnote.model.ts
@@ -0,0 +1,11 @@
+export class createNoteModel {
+    noteId: number;
+    title: String;
+    description: String;
+    color: String;
+    remainder: String;
+    archive: boolean;
+    pin: boolean;
+    isTrash: boolean;
+    userID: number;
+}
